7a54e9dd0e79a67b1c7de625281c563f6c6e0f69
S00235076 <S00235076@S-DP453S3-SL25>
2024-11-11 13:42:37 +0000

    Add project files.

---
+++ ContactManagement/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContactManagement.Data;
+using ContactManager.Models;
+
+namespace ContactManagement
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ContactManagement.Data.ApplicationDbContext _context;
+
+        public DetailsModel(ContactManagement.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Contact Contact { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contact.FirstOrDefaultAsync(m => m.ContactId == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Contact = contact;
+            }
+            return Page();
+        }
+    }
+}
---
+++ ContactManagement/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContactManagement.Data;
+using ContactManager.Models;
+using Microsoft.AspNetCore.Authorization;
+namespace ContactManagement
+{
+    
+    public class IndexModel : PageModel
+    {
+        private readonly ContactManagement.Data.ApplicationDbContext _context;
+
+        public IndexModel(ContactManagement.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Contact> Contact { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Contact = await _context.Contact.ToListAsync();
+        }
+    }
+}
